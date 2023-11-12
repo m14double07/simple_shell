1bf9f7cf5119b00b684eabb2c76d732efb275d06
blessedbest <bestefosa93@gmail.com>
2023-11-11 19:22:21 -0800

    update

---
+++ wait.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int main(void)
+{
+        pid_t pid;
+
+        /* child process is created */
+        pid = fork();
+
+        /* if fork fails */
+        if (pid == -1)
+        {
+                perror("Unsuccessful\n");
+                return i;
+        }
+
+        /* in child process */
+        if (pid == 0)
+        {
+                printf('child process\n');
+        }
+
+        /* in parent proces */
+        else
+        {
+                wait(NULL);
+                sleep (30);
+                printf('Parent Process\n');
+        }
+}
